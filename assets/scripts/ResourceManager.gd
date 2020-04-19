eee327bf61e53323df63541d55bf0301b1a36e56
Simon Berger <simon@siku2.io>
2020-04-19 02:17:24 +0200

    listen to resource remove signal

--- assets/scripts/ResourceManager.gd
+++ assets/scripts/ResourceManager.gd
@@ -10,24 +10,30 @@ const resource_prefab := preload("res://assets/prefabs/Resource.tscn")
 var rng := RandomNumberGenerator.new()
 var resource_count := 0
 
-# Generate a random position whose distance from the origin is between the radius bounds.
-func random_position() -> Vector2:
-	return math_utils.randvec_with_radius_range(rng, spawn_radius_min, spawn_radius_max)
+# handles resources being removed.
+# this means that even if a resource is deleted by something or someone else
+# we can still keep an accurate count of the resources in the world. 
+func _on_Resource_tree_exiting() -> void:
+	print_debug("resource removed")
+	resource_count -= 1
 
 # Spawn a resource at the given position.
 func spawn_resource(pos: Vector2) -> void:
 	var node: Node2D = resource_prefab.instance()
 	node.position = pos
 	
+	# handle removing the resource
+	node.connect("tree_exiting", self, "_on_Resource_tree_exiting")
+	
 	add_child(node)
 	resource_count += 1
-	
-func remove_resource(resource: Node2D) -> void:
-	remove_child(resource)
-	resource_count -= 1
 
 func _ready() -> void:
 	rng.randomize()
+
+# Generate a random position whose distance from the origin is between the radius bounds.
+func random_position() -> Vector2:
+	return math_utils.randvec_with_radius_range(rng, spawn_radius_min, spawn_radius_max)
 
 # Spawn a resource if the resource limit hasn't been reached.
 func maybe_spawn_resource() -> bool:
