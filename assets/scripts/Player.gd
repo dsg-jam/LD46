2e5c7378c95a5a1568c361551e0e7cc09f01b68f
Simon Berger <simon@siku2.io>
2020-04-19 20:23:57 +0200

    building turrets

--- assets/scripts/Player.gd
+++ assets/scripts/Player.gd
@@ -1,6 +1,11 @@
 extends KinematicBody2D
 
 export var speed_mult: float = 80
+export var build_distance: float = 20
+
+export var resources_for_turret := { wood = 0 }
+
+var turret_prefab = preload("res://assets/prefabs/Enemy.tscn")
 
 var interaction_area: Area2D
 
@@ -8,6 +13,7 @@ var interaction_area: Area2D
 var animation_controller
 
 const inventory := {}
+var look_direction: Vector2
 
 func _ready() -> void:
 	interaction_area = $InteractionArea
@@ -23,10 +29,15 @@ func read_input() -> Vector2:
 		input.y += 1
 	if Input.is_action_pressed("player_up"):
 		input.y -= 1
+
 	return input.normalized()
 
 func _physics_process(_delta: float) -> void:
-	var velocity := read_input() * speed_mult
+	var input := read_input()
+	if input.length() > 0:
+		look_direction = input
+	
+	var velocity := input * speed_mult
 	velocity = move_and_slide(velocity)
 	
 	if animation_controller:
@@ -50,9 +61,31 @@ func mine_area() -> void:
 		mine_resource(resource)
 		# only allow mining one resource at a time
 		break
-		
+
+func spawn_turret() -> void:
+	var turret = turret_prefab.instance()
+	turret.position = self.position + look_direction * build_distance
+	get_tree().current_scene.add_child(turret)
+
+func has_item(name: String, count: int) -> bool:
+	return self.inventory.get(name, 0) >= count
+
+func has_items(items: Dictionary) -> bool:
+	for name in items.keys():
+		if !has_item(name, items[name]):
+			return false
+	
+	return true
+
+func try_build_turret() -> void:
+	if has_items(resources_for_turret):
+		spawn_turret()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("player_mine"):
 		mine_area()
+		return
+	
+	if event.is_action_released("player_place_turret"):
+		try_build_turret()
 		return
