f38ee0caec32ffe3ead4cf8f495a6bf77ae8b957
mineiwik <29404520+mineiwik@users.noreply.github.com>
2020-04-19 20:57:05 +0200

    Fixing bullet behaviour

--- assets/scripts/BulletControl.gd
+++ assets/scripts/BulletControl.gd
@@ -6,17 +6,19 @@ export var damage = 10
 var enemy_body
 
 func _physics_process(delta):
-	var dir = (enemy_body.position - position).normalized()
-	var is_collision = move_and_collide(dir*speed*delta)
-	if is_collision:
-		var collider = is_collision.get_collider()
-		if collider.is_in_group("enemies"):
-			if not collider.reduce_health(damage):
-				collider.queue_free()
-			queue_free()
-		elif collider.is_in_group("resources"):
-			collider.remove()
-		
+	if is_instance_valid(enemy_body):
+		var dir = (enemy_body.position - position).normalized()
+		var is_collision = move_and_collide(dir*speed*delta)
+		if is_collision:
+			var collider = is_collision.get_collider()
+			if collider.is_in_group("enemies"):
+				if not collider.reduce_health(damage):
+					collider.queue_free()
+				queue_free()
+			elif collider.is_in_group("resources"):
+				collider.remove()
+	else:
+		queue_free()
 
 
 func _on_Timer_timeout():
