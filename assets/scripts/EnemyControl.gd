bce974aabfa3f63c6610d3a17f05f81d28137474
mineiwik <29404520+mineiwik@users.noreply.github.com>
2020-04-19 11:46:00 +0200

    Generating enemy with random texture

--- assets/scripts/EnemyControl.gd
+++ assets/scripts/EnemyControl.gd
@@ -3,16 +3,21 @@ extends KinematicBody2D
 var health = 100
 var damage = 1
 var speed = 50
+var rng = RandomNumberGenerator.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var count_frames = $Sprite.get_hframes() + $Sprite.get_vframes()
+	rng.randomize()
+	var select_frame = rng.randi_range(0, count_frames-1)
+	print(select_frame)
+	$Sprite.set_frame(select_frame)
 	pass
 
 
 
 func _physics_process(delta):
-	
 
 	var dir = ($"../Companion".position - position).normalized()
 	var is_collision = move_and_collide(dir*speed*delta)
