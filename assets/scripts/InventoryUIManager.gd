1b3564ab497e2a8ec7ec02b87ea3eb68dc3e3c00
Simon Berger <simon@siku2.io>
2020-04-20 16:31:16 +0200

    improve inventory ui

--- assets/scripts/InventoryUIManager.gd
+++ assets/scripts/InventoryUIManager.gd
@@ -1,15 +1,14 @@
-extends VBoxContainer
+extends Node
 
 var labels = {}
 
 func find_label(name: String) -> Label:
-	return get_node("Item %s/Count" % name) as Label
+	return get_node("Values/%s" % name) as Label
 
 func _ready():
 	labels["wood"] = find_label("Wood")
 	labels["stone"] = find_label("Stone")
 	labels["iron"] = find_label("Iron")
-
 
 func update_item_count(item: String, count: int) -> void:
 	var label: Label = labels[item]
