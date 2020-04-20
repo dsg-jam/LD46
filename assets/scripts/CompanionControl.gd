b84233994324429210c66396ea3ded438e2e275b
mineiwik <29404520+mineiwik@users.noreply.github.com>
2020-04-20 16:38:46 +0200

    Added GameOver

--- assets/scripts/CompanionControl.gd
+++ assets/scripts/CompanionControl.gd
@@ -13,9 +13,12 @@ func _ready():
 func reduce_health(amount):
 	health -= amount
 	if health <= 0:
+		game_over()
 		return false
 	return true
 
+func game_over():
+	$"../GameOver/GameOverUI".visible = true
 
 func _physics_process(_delta):
 	#var dir = Vector2(rng.randi_range(-10,10), rng.randi_range(-10,10)).normalized()
---
+++ assets/scripts/GameOverControl.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_TryAgainButton_pressed():
+	get_tree().reload_current_scene()
+
+
+
+func _on_ExitButton_pressed():
+	# Go back to Main Menu
+	pass # Replace with function body.
